56d1c67e149a51c055b05d01ee6937e161483115
DakshAggarwal <dakshaggarwal.unofficial1996@gmail.com>
2016-09-02 22:21:39 -0700

    Checked Exception

---
+++ CheckedExceptionDemo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class CheckedExceptionDemo{
+static public void main() throws IOException{
+
+InputStreamReader input = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(input);
+
+//Scanner sc = new Scanner(System.in);
+
+String msg = br.readLine();
+
+}
+
+}
